22d68929c29b29192c75f988e6e2e57296fa02d0
nicomaza <nicolasmaza66@hotmail.com>
2024-08-13 12:06:13 -0300

    agrego cors

--- cordobacelulares/src/main/java/celulares/cordobacelulares/controller/BrandController.java
+++ cordobacelulares/src/main/java/celulares/cordobacelulares/controller/BrandController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+@CrossOrigin
 @RestController
 @RequestMapping("/api/brand")
 public class BrandController {
