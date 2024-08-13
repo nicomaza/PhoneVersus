c68476d83e65cc3139a0b775e282ac4de0840af9
nicomaza <nicolasmaza66@hotmail.com>
2024-08-13 14:09:34 -0300

    cambio url

--- cordobacelulares/AngularBoostrapCordobaCelulares/cordobacelulares/src/app/services/brand.service.spec.ts
+++ cordobacelulares/AngularBoostrapCordobaCelulares/cordobacelulares/src/app/services/brand.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { BrandService } from '../brand.service';
+import { BrandService } from './brand.service';
 
 describe('BrandService', () => {
   let service: BrandService;
