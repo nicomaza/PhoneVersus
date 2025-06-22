e1fcd17328d508832ab79cd8d180592c59ef1474
Maza Victor Nicolas <nicolasmaza66@gmail.com>
2025-06-21 22:22:33 -0300

    suscribe y fechas dde garantia

--- cordobacelulares/AngularBoostrapCordobaCelulares/cordobacelulares/src/app/comparatives/comparatives.component.ts
+++ cordobacelulares/AngularBoostrapCordobaCelulares/cordobacelulares/src/app/comparatives/comparatives.component.ts
@@ -3,7 +3,16 @@ import { Phone } from '../models/phone';
 import { CommonModule } from '@angular/common';
 import { PhonesService } from '../services/phones.service';
 import { IonicModule } from '@ionic/angular';
-import { catchError, debounceTime, distinctUntilChanged, finalize, map, Observable, of, switchMap } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  finalize,
+  map,
+  Observable,
+  of,
+  switchMap,
+} from 'rxjs';
 import { searchedPhone } from '../models/searchedPhone';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -13,14 +22,14 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, IonicModule, FormsModule, RouterLink],
   templateUrl: './comparatives.component.html',
-  styleUrl: './comparatives.component.css'
+  styleUrl: './comparatives.component.css',
 })
 export class ComparativesComponent implements OnInit {
   @Input('id') idphone!: string;
 
   phoneleft: Phone | undefined = undefined;
 
-  phoneright: Phone | undefined = undefined;;
+  phoneright: Phone | undefined = undefined;
   IDleft: number = 0;
   IDright: number = 0;
 
@@ -30,30 +39,30 @@ export class ComparativesComponent implements OnInit {
   rightclass = false;
   leftclass = false;
 
-
   phones$: Observable<searchedPhone[]> = of([]);
   isLoading: boolean = false;
   isLoadingRight: boolean = false;
 
-  constructor(private phoneservice: PhonesService, private cdr: ChangeDetectorRef) { }
+  constructor(
+    private phoneservice: PhonesService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    
-   this.setPhoneLeft(parseInt(this.idphone))
-   if (this.idphone) {
     this.setPhoneLeft(parseInt(this.idphone));
-  }
+    if (this.idphone) {
+      this.setPhoneLeft(parseInt(this.idphone));
+    }
   }
 
-  deletePhoneLeft(){
+  deletePhoneLeft() {
     this.phoneleft = undefined;
     this.cdr.detectChanges(); // Detect changes
   }
-  deletePhoneRight(){
+  deletePhoneRight() {
     this.phoneright = undefined;
     this.cdr.detectChanges(); // Detect changes
   }
-
 
   setPhoneLeft(id: number) {
     this.phoneservice.getPhoneById(id).subscribe(
@@ -62,34 +71,29 @@ export class ComparativesComponent implements OnInit {
         this.phoneleft = data;
       },
       (error) => {
-        console.log(error)
+        console.log(error);
       }
-    )
+    );
   }
- 
 
   setPhoneRight(id: number) {
     this.phoneservice.getPhoneById(id).subscribe(
       (data: Phone) => {
         this.phones$ = of([]);
         this.phoneright = data;
-
       },
       (error) => {
-        console.log(error)
+        console.log(error);
       }
-    )
+    );
   }
 
-  isBothLoaded(){
-
-   
-    if(this.phoneleft == undefined){
-        this.leftclass = true
-        
-     }else if(this.phoneright == undefined){
-        this.rightclass = true
-             }
+  isBothLoaded() {
+    if (this.phoneleft == undefined) {
+      this.leftclass = true;
+    } else if (this.phoneright == undefined) {
+      this.rightclass = true;
+    }
   }
 
   isPhoneLeftLoaded(): boolean {
@@ -97,21 +101,26 @@ export class ComparativesComponent implements OnInit {
   }
 
   isPhoneRightLoaded(): boolean {
-
-    
     return !this.phoneright;
   }
-
 
   getRouterLink(): string[] | null {
     if (this.phoneleft?.idPhone && this.phoneright?.idPhone) {
       // Verificar si el ID fue pasado por @Input y ajustar la ruta
-      return this.idphone ? ['../../versus', this.phoneleft.idPhone.toString(), this.phoneright.idPhone.toString()]
-                          : ['../versus', this.phoneleft.idPhone.toString(), this.phoneright.idPhone.toString()];
+      return this.idphone
+        ? [
+            '../../versus',
+            this.phoneleft.idPhone.toString(),
+            this.phoneright.idPhone.toString(),
+          ]
+        : [
+            '../versus',
+            this.phoneleft.idPhone.toString(),
+            this.phoneright.idPhone.toString(),
+          ];
     }
     return null;
   }
-  
 
   onSearch(value: string): void {
     if (value.trim() === '') {
@@ -126,13 +135,14 @@ export class ComparativesComponent implements OnInit {
       distinctUntilChanged(), // Evita búsquedas repetidas para el mismo valor
       switchMap((searchText: string) =>
         this.phoneservice.phoneSearcher(searchText).pipe(
-          map((phones: searchedPhone[]) => phones.map(phone => ({
-            ...phone,
-            // Formatear los datos según sea necesario
-            brand: phone.brand.toUpperCase(), // Ejemplo: poner en mayúsculas
-            // Ejemplo: poner en minúsculas
-
-          }))),
+          map((phones: searchedPhone[]) =>
+            phones.map((phone) => ({
+              ...phone,
+              // Formatear los datos según sea necesario
+              brand: phone.brand.toUpperCase(), // Ejemplo: poner en mayúsculas
+              // Ejemplo: poner en minúsculas
+            }))
+          ),
           catchError(() => of([])), // Maneja errores devolviendo un array vacío
           finalize(() => {
             this.isLoadingRight = false;
@@ -142,10 +152,8 @@ export class ComparativesComponent implements OnInit {
       )
     );
 
-    this.phones$.subscribe((data) => console.log(data))
+    this.phones$.subscribe((data) => console.log(data));
   }
-
-
 
   onSearchRight(value: string): void {
     if (value.trim() === '') {
@@ -160,13 +168,14 @@ export class ComparativesComponent implements OnInit {
       distinctUntilChanged(), // Evita búsquedas repetidas para el mismo valor
       switchMap((searchText: string) =>
         this.phoneservice.phoneSearcher(searchText).pipe(
-          map((phones: searchedPhone[]) => phones.map(phone => ({
-            ...phone,
-            // Formatear los datos según sea necesario
-            brand: phone.brand.toUpperCase(), // Ejemplo: poner en mayúsculas
-            // Ejemplo: poner en minúsculas
-
-          }))),
+          map((phones: searchedPhone[]) =>
+            phones.map((phone) => ({
+              ...phone,
+              // Formatear los datos según sea necesario
+              brand: phone.brand.toUpperCase(), // Ejemplo: poner en mayúsculas
+              // Ejemplo: poner en minúsculas
+            }))
+          ),
           catchError(() => of([])), // Maneja errores devolviendo un array vacío
           finalize(() => {
             this.isLoading = false;
@@ -176,9 +185,10 @@ export class ComparativesComponent implements OnInit {
       )
     );
 
-    this.phones$.subscribe((data) => )
+    this.phones$.subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
 
 /**
@@ -187,7 +197,7 @@ export class ComparativesComponent implements OnInit {
                                 style="width: 200; height: 400;" />
                         </div> */
 /**
- * 
+ *
  */
 
 /**
